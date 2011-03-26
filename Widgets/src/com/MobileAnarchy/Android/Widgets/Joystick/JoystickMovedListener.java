3e490fda078688b2e302da1c99eebfc1db0a79b0
declanshanaghy <declanshanaghy@gmail.com>
2011-03-26 01:02:32 +0000

    New joystick features: Circular movement restriction Click handling User settable range Invert Y axis Callback sensitivity threshold

--- Widgets/src/com/MobileAnarchy/Android/Widgets/Joystick/JoystickMovedListener.java
+++ Widgets/src/com/MobileAnarchy/Android/Widgets/Joystick/JoystickMovedListener.java
@@ -1,8 +1,6 @@
 package com.MobileAnarchy.Android.Widgets.Joystick;
 
 public interface JoystickMovedListener {
-
 	public void OnMoved(int pan, int tilt);
 	public void OnReleased();
-	
 }
--- Widgets/src/com/MobileAnarchy/Android/Widgets/Joystick/JoystickView.java
+++ Widgets/src/com/MobileAnarchy/Android/Widgets/Joystick/JoystickView.java
@@ -7,24 +7,67 @@ import android.graphics.Paint;
 import android.os.Handler;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.HapticFeedbackConstants;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class JoystickView extends View {
-
 	// =========================================
 	// Private Members
 	// =========================================
-
+	private final boolean D = false;
 	private final String TAG = "JoystickView";
+	
+	private Paint dbgPaint1;
+	private Paint dbgPaint2;
+	
 	private Paint circlePaint;
 	private Paint handlePaint;
-	private double touchX, touchY;
 	private int innerPadding;
+	private int radius;
 	private int handleRadius;
 	private int handleInnerBoundaries;
-	private JoystickMovedListener listener;
-	private int sensitivity;
+	private JoystickMovedListener moveListener;
+	private JoystickClickedListener clickListener;
+
+	//# of pixels movement required between reporting to the listener
+	private float moveResolution;
+
+	private boolean yAxisInverted;
+	
+	//Max range of movement in user coordinate system
+	public final static int CONSTRAIN_BOX = 0;
+	public final static int CONSTRAIN_CIRCLE = 1;
+	private int movementConstraint;
+	private float movementRange;
+	private float movementRangeH;
+
+	//Records touch pressure for click handling
+	private float touchPressure;
+	private boolean clicked;
+	private float clickThreshold;
+	
+	//Last touch point in view coordinates
+	private float touchX, touchY;
+	
+	//Last reported position in view coordinates (allows different reporting sensitivities)
+	private float reportX, reportY;
+	
+	//Handle center in view coordinates
+	private float handleX, handleY;
+	
+	//Center of the view in view coordinates
+	private int px, py;
+
+	//Size of the view in view coordinates
+	private int dx, dy;
+
+	//Cartesian coordinates of last touch point - joystick center is (0,0)
+	private int cartX, cartY;	//Can be commented out if optimization is needed
+	
+	//Polar coordinates of the touch point from joystick center
+	private double radial;
+	private float angle; 			//(can be commented out if optimization is needed)
 
 	// =========================================
 	// Constructors
@@ -52,6 +95,16 @@ public class JoystickView extends View {
 	private void initJoystickView() {
 		setFocusable(true);
 
+		dbgPaint1 = new Paint(Paint.ANTI_ALIAS_FLAG);
+		dbgPaint1.setColor(Color.RED);
+		dbgPaint1.setStrokeWidth(1);
+		dbgPaint1.setStyle(Paint.Style.STROKE);
+		
+		dbgPaint2 = new Paint(Paint.ANTI_ALIAS_FLAG);
+		dbgPaint2.setColor(Color.GREEN);
+		dbgPaint2.setStrokeWidth(1);
+		dbgPaint2.setStyle(Paint.Style.STROKE);
+		
 		circlePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
 		circlePaint.setColor(Color.GRAY);
 		circlePaint.setStrokeWidth(1);
@@ -63,15 +116,74 @@ public class JoystickView extends View {
 		handlePaint.setStyle(Paint.Style.FILL_AND_STROKE);
 
 		innerPadding = 10;
-		sensitivity = 10;
+		
+		setMovementRange(10);
+		setMoveResolution(1.0f);
+		setClickThreshold(0.9f);
+		setYAxisInverted(true);
 	}
 
+	public void setMovementConstraint(int movementConstraint) {
+		if (movementConstraint < CONSTRAIN_BOX || movementConstraint > CONSTRAIN_CIRCLE)
+			Log.e(TAG, "invalid value for movementConstraint");
+		else
+			this.movementConstraint = movementConstraint;
+	}
+	
+	public int getMovementConstraint() {
+		return movementConstraint;
+	}
+	
+	public boolean isYAxisInverted() {
+		return yAxisInverted;
+	}
+	
+	public void setYAxisInverted(boolean yAxisInverted) {
+		this.yAxisInverted = yAxisInverted;
+	}
+	
+	/**
+	 * Set the pressure sensitivity for registering a click
+	 * @param clickThreshold threshold 0...1.0f inclusive. 0 will cause clicks to never be reported, 1.0 is a very hard click
+	 */
+	public void setClickThreshold(float clickThreshold) {
+		if (clickThreshold < 0 || clickThreshold > 1.0f)
+			Log.e(TAG, "clickThreshold must range from 0...1.0f inclusive");
+		else
+			this.clickThreshold = clickThreshold;
+	}
+	
+	public float getClickThreshold() {
+		return clickThreshold;
+	}
+	
+	public void setMovementRange(float movementRange) {
+		this.movementRange = movementRange;
+		this.movementRangeH = movementRange / 2;
+	}
+	
+	public float getMovementRange() {
+		return movementRange;
+	}
+	
+	public void setMoveResolution(float moveResolution) {
+		this.moveResolution = moveResolution;
+	}
+	
+	public float getMoveResolution() {
+		return moveResolution;
+	}
+	
 	// =========================================
 	// Public Methods 
 	// =========================================
 
 	public void setOnJostickMovedListener(JoystickMovedListener listener) {
-		this.listener = listener;
+		this.moveListener = listener;
+	}
+	
+	public void setOnJostickClickedListener(JoystickClickedListener listener) {
+		this.clickListener = listener;
 	}
 	
 	// =========================================
@@ -88,6 +200,12 @@ public class JoystickView extends View {
 		handleRadius = (int)(d * 0.25);
 		handleInnerBoundaries = handleRadius;
 		
+		px = d / 2;
+		py = d / 2;
+		dx = d;
+		dy = d;
+		radius = Math.min(px, py) - handleInnerBoundaries;
+
 		setMeasuredDimension(d, d);
 	}
 
@@ -117,40 +235,121 @@ public class JoystickView extends View {
 		canvas.drawCircle(px, py, radius - innerPadding, circlePaint);
 
 		// Draw the handle
-		canvas.drawCircle((int) touchX + px, (int) touchY + py, handleRadius,
-				handlePaint);
+		handleX = touchX + px;
+		handleY = touchY + py;
+		canvas.drawCircle(handleX, handleY, handleRadius, handlePaint);
 
+		if (D) {
+			canvas.drawCircle(handleX, handleY, 3, dbgPaint1);
+			
+			if ( movementConstraint == CONSTRAIN_CIRCLE ) {
+				canvas.drawCircle(px, py, this.radius, dbgPaint1);
+			}
+			else {
+				canvas.drawRect(px-handleRadius, py-handleRadius, px+handleRadius, py+handleRadius, dbgPaint1);
+			}
+			
+			//Origin to touch point
+			canvas.drawLine(px, py, handleX, handleY, dbgPaint2);
+			
+			canvas.drawText(String.format("(%d,%d)", cartX, cartY), handleX-20, handleY-7, dbgPaint2);
+			canvas.drawText("("+ String.format("%.0f, %.1f", radial, angle) + (char) 0x00B0 + ")", handleX-20, handleY+15, dbgPaint2);
+		}
+
+//		Log.d(TAG, String.format("touch(%f,%f)", touchX, touchY));
+//		Log.d(TAG, String.format("onDraw(%.1f,%.1f)\n\n", handleX, handleY));
 		canvas.save();
 	}
 
+	// Constrain touch within a box
+	@SuppressWarnings("unused")
+	private void constrainBox() {
+		touchX = Math.max(Math.min(touchX, radius), -radius);
+		touchY = Math.max(Math.min(touchY, radius), -radius);
+	}
+
+	// Constrain touch within a circle
+	private void constrainCircle() {
+		float diffX = touchX;
+		float diffY = touchY;
+		double radial = Math.sqrt((diffX*diffX) + (diffY*diffY));
+		if ( radial > radius ) {
+			touchX = (int)((diffX / radial) * radius);
+			touchY = (int)((diffY / radial) * radius);
+		}
+	}
+	
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		int actionType = event.getAction();
 		if (actionType == MotionEvent.ACTION_MOVE) {
-			int px = getMeasuredWidth() / 2;
-			int py = getMeasuredHeight() / 2;
-			int radius = Math.min(px, py) - handleInnerBoundaries;
-
+			// Translate touch position to center of view
 			touchX = (event.getX() - px);
-			touchX = Math.max(Math.min(touchX, radius), -radius);
-
 			touchY = (event.getY() - py);
-			touchY = Math.max(Math.min(touchY, radius), -radius);
 
-			// Coordinates
-			Log.d(TAG, "X:" + touchX + "|Y:" + touchY);
-
-			// Pressure
-			if (listener != null) {
-				listener.OnMoved((int) (touchX / radius * sensitivity), (int) (touchY  / radius * sensitivity));
-			}
-
+			reportOnMoved();
 			invalidate();
-		} else if (actionType == MotionEvent.ACTION_UP) {
+			
+			touchPressure = event.getPressure();
+			reportOnPressure();
+		} 
+		else if (actionType == MotionEvent.ACTION_UP) {
 			returnHandleToCenter();
-			Log.d(TAG, "X:" + touchX + "|Y:" + touchY);
+			//Log.d(TAG, "X:" + touchX + "|Y:" + touchY);
 		}
 		return true;
+	}
+
+	private void reportOnMoved() {
+		if ( movementConstraint == CONSTRAIN_CIRCLE )
+			constrainCircle();
+		else
+			constrainBox();
+
+		cartX = toUserCoordinates(touchX);
+		cartY = toUserCoordinates(touchY);
+		if ( !yAxisInverted )
+			cartY  *= -1;
+		
+		//Other calcs which aren't absolutely necessary 
+		radial = Math.sqrt((cartX*cartX) + (cartY*cartY));
+		angle = (int)(Math.atan2(cartY, cartX) * 57.2957795);
+
+		if (moveListener != null) {
+			boolean rx = Math.abs(touchX - reportX) >= moveResolution;
+			boolean ry = Math.abs(touchY - reportY) >= moveResolution;
+			if (rx || ry) {
+				this.reportX = touchX;
+				this.reportY = touchY;
+				
+//				Log.d(TAG, String.format("moveListener.OnMoved(%d,%d)", (int)cartX, (int)cartY));
+				moveListener.OnMoved(cartX, cartY);
+			}
+		}
+	}
+	
+	private int toUserCoordinates(float viewCoordinate) {
+		return (int)(viewCoordinate / radius * movementRange);
+	}
+
+	//Simple pressure click
+	private void reportOnPressure() {
+//		Log.d(TAG, String.format("touchPressure=%.2f", this.touchPressure));
+		if ( clickListener != null ) {
+			if ( clicked && touchPressure < clickThreshold ) {
+				clickListener.OnReleased();
+				this.clicked = false;
+//				Log.d(TAG, "reset click");
+				invalidate();
+			}
+			else if ( !clicked && touchPressure >= clickThreshold ) {
+				clicked = true;
+				clickListener.OnClicked();
+//				Log.d(TAG, "click");
+				invalidate();
+				performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+			}
+		}
 	}
 
 	private void returnHandleToCenter() {
@@ -166,13 +365,15 @@ public class JoystickView extends View {
 				public void run() {
 					touchX += intervalsX;
 					touchY += intervalsY;
+					
+					reportOnMoved();
 					invalidate();
 				}
 			}, i * 40);
 		}
 
-		if (listener != null) {
-			listener.OnReleased();
+		if (moveListener != null) {
+			moveListener.OnReleased();
 		}
 	}
 }
